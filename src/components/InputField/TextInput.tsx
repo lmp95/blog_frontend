8be716f9962ce100eb6fbfef84e311e0d2aeee6a
Lwin Maung Phyo <lwinmgphyo95@gmail.com>
2023-08-24 22:03:08 +0630

    added sign up page

--- src/components/InputField/TextInput.tsx
+++ src/components/InputField/TextInput.tsx
@@ -2,7 +2,7 @@ import { ComponentPropsWithoutRef, HTMLInputTypeAttribute } from 'react';
 
 function TextInput({ label, type = 'text' }: TextInputProps) {
     return (
-        <div className='mb-2'>
+        <div className='my-2'>
             <p className='text-sm pb-1'>{label}</p>
             <input type={type} className='p-2 text-sm w-full border-[0.5px] border-lightDark/70 rounded-md' />
         </div>
@@ -13,5 +13,5 @@ export default TextInput;
 
 interface TextInputProps extends ComponentPropsWithoutRef<'input'> {
     label: string;
-    type: HTMLInputTypeAttribute;
+    type?: HTMLInputTypeAttribute;
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,13 @@
+import { Dispatch, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FilledButton } from '~/components/Button';
 import { TextInputField } from '~/components/InputField';
 
 function Login() {
+    const [isRegister, setIsRegister] = useState<boolean>(false);
     const navigate = useNavigate();
+
+    if (isRegister) return <SignUp setIsRegister={setIsRegister} />;
 
     return (
         <div className='grid grid-cols-2 place-items-center w-screen h-screen '>
@@ -11,16 +15,12 @@ function Login() {
             <div className='px-16 grid items-center justify-start border-[1px] border-lightDark/10 w-full h-full'>
                 <div className='flex flex-col gap-3 w-[500px]'>
                     <h1 className='text-h3 mb-3 font-semibold text-matteBlack'>Log In</h1>
-                    <div>
-                        <TextInputField label='Email' type='email' />
-                    </div>
-                    <div className='mb-2'>
-                        <TextInputField label='Password' type='password' />
-                    </div>
+                    <TextInputField label='Email' type='email' />
+                    <TextInputField label='Password' type='password' />
                     <FilledButton label='Login' isFullWidth onClick={() => navigate('/')} />
                     <p className='text-sm'>
                         Don't have an account?{' '}
-                        <span onClick={() => navigate('/signUp')} className='text-primary cursor-pointer'>
+                        <span onClick={() => setIsRegister(true)} className='text-primary cursor-pointer'>
                             Sign Up
                         </span>
                     </p>
@@ -31,3 +31,33 @@ function Login() {
 }
 
 export default Login;
+
+export function SignUp({ setIsRegister }: SignUpProps) {
+    const navigate = useNavigate();
+
+    return (
+        <div className='grid grid-cols-2 place-items-center w-screen h-screen '>
+            <div className='bg-primary w-full h-full'></div>
+            <div className='px-16 grid items-center justify-start border-[1px] border-lightDark/10 w-full h-full'>
+                <div className='flex flex-col gap-3 w-[500px]'>
+                    <h1 className='text-h3 mb-3 font-semibold text-matteBlack'>Sign Up</h1>
+                    <TextInputField label='Username' />
+                    <TextInputField label='Email' type='email' />
+                    <TextInputField label='Password' type='password' />
+                    <TextInputField label='Confirm Password' type='password' />
+                    <FilledButton label='Create an account' isFullWidth onClick={() => null} />
+                    <p className='text-sm'>
+                        Already have an account?{' '}
+                        <span onClick={() => setIsRegister(false)} className='text-primary cursor-pointer'>
+                            Login
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+interface SignUpProps {
+    setIsRegister: Dispatch<React.SetStateAction<boolean>>;
+}
